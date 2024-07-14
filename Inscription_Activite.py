d13da0a62994f6279238e7bb753d2c07590124c9
AsmaAitOuakli <156623308+AsmaAitOuakli@users.noreply.github.com>
2024-07-13 22:38:02 -0400

    commit 1 inscription activite

---
+++ Inscription_Activite.py
@@ -0,0 +1,5 @@
+class Inscription_Activite:
+    def __init__(self,code_Activie, Id_Utilisateur, date):
+        self.code_Activie=code_Activie
+        self.Id_Utilisateur=Id_Utilisateur
+        self.date=date
